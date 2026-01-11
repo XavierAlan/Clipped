e830ec0d7a4ff536ffbac0260347cfe8130009ed
Alan Xavier <100752217+XavierAlan@users.noreply.github.com>
2026-01-11 13:05:59 +0000

    Atualização da Webpage no Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Te amo, meu amor, se você me ama, hoje será o primeiro dia do resto das
-      nossas vidas
-    </h1>
-  );
+  return <h1>EM CONSTRUÇÃO</h1>;
 }
 
 export default Home;
