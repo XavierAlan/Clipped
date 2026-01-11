7603df6e91ba347cf6b4102d2abbf1afa3a695c0
Alan Xavier <100752217+XavierAlan@users.noreply.github.com>
2026-01-11 02:29:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home (){
-    return <h1>Te amo, meu amor, se você me ama, hoje será o primeiro dia do resto das nossas vidas</h1>
+function Home() {
+  return (
+    <h1>
+      Te amo, meu amor, se você me ama, hoje será o primeiro dia do resto das
+      nossas vidas
+    </h1>
+  );
 }
 
 export default Home;
